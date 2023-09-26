7798a33742f3215e04e03c37cad8743d32d89cbc
Laura <Laura@DESKTOP-BESSB6E>
2023-09-26 23:45:37 +0300

    Add project files.

---
+++ 1.5Uzduotis_List/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1._5Uzduotis_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 4, 5, 6 };
+
+        
+            foreach (int i in GetFactorials(list))
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        public static List<int> GetFactorials(List<int> list)
+        {
+            List<int> fac = new List<int>();
+            
+            foreach (int number in list)
+            {
+                int factorials = 1;
+
+
+                for (int i = 1; i <= number; i++)
+                {
+                factorials *= i;
+                }
+                fac.Add(factorials);
+            }
+            return fac;
+        }
+    }
+}
